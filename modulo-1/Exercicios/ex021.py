4b5ec1b519b4f1a1f264f5aa7d03bf36eefbdc5d
Fernando Neumann de Toledo <fernando.neumannt@gmail.com>
2022-02-10 17:51:21 -0300

    go go go

---
+++ modulo-1/Exercicios/ex021.py
@@ -0,0 +1,9 @@
+'''
+Faça um programa que abra e reproduza o áudio de um arquivo MP3.
+'''
+import pygame
+pygame.mixer.init()
+pygame.init()
+pygame.mixer.music.load('ex021.mp3')
+pygame.mixer.music.play(loops=0, start=0.0)
+pygame.event.wait()
