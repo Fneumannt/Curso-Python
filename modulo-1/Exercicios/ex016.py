d6b50043bcae10c1d0ccd4b73ed2b477f6d8814c
Fernando Neumann de Toledo <fernando.neumannt@gmail.com>
2022-02-10 15:53:23 -0300

    exercicios

---
+++ modulo-1/Exercicios/ex016.py
@@ -0,0 +1,14 @@
+from math import trunc
+num = float(input('Digite um valor: '))
+print('O valor digitado foi {}, e a sua porção inteira é {}'.format(num, trunc(num)))
+
+'''
+import math
+num = float(input('Digite um valor: '))
+print('O valor digitado foi {}, e sua porção inteira é {}'.format(num, math.trunc(num)))
+'''
+
+'''
+num = float(input('Digite um valor: '))
+print('O valor digitado foi de {}, e sua porção inteira é {}'.format(num, int(num))
+'''
---
+++ modulo-1/Exercicios/ex017.py
@@ -0,0 +1,25 @@
+'''
+Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um
+triangulo retangulo, calcule e mostre o comprimento da hipotenusa.
+'''
+
+co = float(input('Comprimento do cateto oposto: '))
+ca = float(input('Comprimento do cateto adjacente: '))
+hi = (co**2 + ca**2) ** (1/2)
+print('A hipotenusa vai medir {:.2f}'.format(hi))
+
+'''
+import math
+co = float(input('Comprimento do cateto oposto: '))
+ca = float(input('Comprimento do cateto adjacente: '))
+hi = math.hypot(co, ca)
+print('A hipotenusa vai medir {:.2f}'.format(hi))
+'''
+
+'''
+from math import hypot
+co = float(input('Comprimento do cateto oposto: '))
+ca = float(input('Comprimento do cateto adjacente: '))
+hi = hypot(co, ca)
+print('A hipotenusa vai medir {:.2f}'.format(hi))
+'''
