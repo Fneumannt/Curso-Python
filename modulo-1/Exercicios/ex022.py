aecf8a1af7d1e8d82ab4f828eb71b1751e2bbf39
Fernando Neumann de Toledo <fernando.neumannt@gmail.com>
2022-02-14 21:13:07 -0300

    upp

---
+++ modulo-1/Exercicios/ex022.py
@@ -0,0 +1,5 @@
+nome = input('Digite o seu nome completo: ')
+print(nome.upper())
+print(nome.lower())
+separado = nome.split()
+print(len(separado[0]))
---
+++ modulo-1/Exercicios/ex023.py
@@ -0,0 +1,3 @@
+numero = input('Digite um numero de 0 a 9999: ')
+unidade = len(numero)
+print(Unidade)
