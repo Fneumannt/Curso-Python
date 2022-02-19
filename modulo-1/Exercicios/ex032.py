9b56a63fecffbf7fb2a98a48a048f2cb20aa2c73
Fernando Neumann de Toledo <fernando.neumannt@gmail.com>
2022-02-19 18:52:19 -0300

    Update ex032.py

--- modulo-1/Exercicios/ex032.py
+++ modulo-1/Exercicios/ex032.py
@@ -2,7 +2,10 @@
 Faça um programa que leia um ano qualquer
 e mostre se ele é bissexto.
 '''
-ano = int(input('Que ano quer analisar? '))
+from datetime import date
+ano = int(input('Que ano quer analisar? Coloque 0 para analisar o ano atual: '))
+if ano == 0:
+    ano = date.today().year
 if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
     print('O ano {} é BISSEXTO'.format(ano))
 else:
